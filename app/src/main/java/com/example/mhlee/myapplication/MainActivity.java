747685e328e8faf742130a1109ded79031c00f03
mhlee1215@gmail.com <mhlee1215@gmail.com>
2016-01-17 00:46:28 -0800

    the second commit

--- app/src/main/java/com/example/mhlee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mhlee/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
-        System.out.println("Hello button");
+        System.out.println("Hello button2");
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
